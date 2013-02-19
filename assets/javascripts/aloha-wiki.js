edda4b26a543cfaf7029e869d67105fcbbe88f84
dever860 <dever860@gmail.com>
2013-02-18 23:33:38 -0500

    Trying to make toolbar less annoying by having it pinned to the top of the DIV while scrolling, need more work.
    Ref:
    https://gist.github.com/evo42/3508356

--- assets/javascripts/aloha-wiki.js
+++ assets/javascripts/aloha-wiki.js
@@ -2,6 +2,20 @@ Aloha.ready( function() {
 	var $ = Aloha.jQuery;
 //	Aloha.jQuery('#editor').aloha();
     $('#editor').aloha();
+    Aloha.require(['jquery', 'ui/scopes', 'ui/ui-plugin'], function ($, Scopes, UiPlugin) {
+        var toolbar = $('.aloha-toolbar');
+        // Center and resize the toolbar
+        //toolbar.css({'position': 'static', 'margin': 'auto'});
+        $('#editor').append(toolbar);
+        Scopes.setScope('Aloha.continuoustext');
+        UiPlugin.showToolbar();
+        // The child element must also be resized, don't know why
+        toolbar.children().css({'width': 600});
+ 
+        Aloha.bind('aloha-editable-deactivated', function () {
+            UiPlugin.showToolbar();
+        });
+    });
 
     $('#save').click(function(){
                payload = $("#editor");
